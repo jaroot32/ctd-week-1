3e1f3d1059138afc46436f51722c13d4f5e8a7a6
Jonathan Root <jonathanroot@Jonathans-MacBook-Pro-2.local>
2025-07-15 23:47:19 -0400

    simple todo and setup configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    settings: { react: { version: 'detect' } },
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
@@ -23,7 +25,8 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn', //this changes the error to a warning
+      'react/prop-types': 'off', //this suppresses warnings about not using prop-types
     },
   },
-])
+]);
