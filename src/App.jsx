048341d68e0d25dc1ced8204295ea2054b4caf82
Jonathan Root <jonathanroot@Jonathans-MacBook-Pro-2.local>
2025-08-06 00:47:20 -0400

    removed elements from return statement, changed useState variable names and initialized with an empty array

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function App() {
-  const [newTodo, setNewToDo] = useState('initialValue');
+  const [todoList, setTodoList] = useState([]);
   return (
     <div>
-      <h1>Todo List</h1>
+      {/* <h1>Todo List</h1>
       <TodoForm />
       {newTodo}
-      <TodoList />
+      <TodoList /> */}
     </div>
   );
 }
